da1c4f161f7268253e1763acc489320d213812c3
asta345 <81383112+asta345@users.noreply.github.com>
2021-10-03 16:31:27 +0530

    deleting the cart item and rendering the list using arrays

--- src/Cart.js
+++ src/Cart.js
@@ -36,15 +36,52 @@ class Cart extends React.Component {
 		// this.increaseQuantity = this.increaseQuantity.bind(this);
 		//  this.testing();
 	      }
+	      //to incrse the quantity of the cart item
+	      handleIncreseQuantity =(product)=>{
+		console.log('heyyplease increse the product ',product);
+		const{products}=this.state;
+		const index = products.indexOf(product);
+		products[index].qty+=1;
+		this.setState({
+			products
+		})
+		}
+		handleDecreaseQuantity =(product)=>{
+			console.log('heyyplease decrese the product ',product);
+			const{products}=this.state;
+			const index = products.indexOf(product);
+			if(products[index].qty===0){
+				return
+			}
+			products[index].qty-=1;
+
+			this.setState({
+				products
+			})
+			}
+
+		handleDeleteProduct =(id)=>{
+			
+			const {products}=this.state;
+			const items = products.filter((item) => item.id !== id); // [{}]
+
+			this.setState({
+			  products: items
+			})
+			
+		}
 	render(){
-		const {products}=this.state	
+	       const {products}=this.state
 	return(
 		
           <div class="cart">
 		  {products.map((product)=>{
 			  return  (<CartItem
 				 product={product}
-				 key={product.id}/>)
+				 key={product.id}
+				 onIncreaseQuantity={this.handleIncreseQuantity}
+				 onDecreaseQuantity={this.handleDecreaseQuantity}
+				 onDeleteProduct={this.handleDeleteProduct}  />)
 		  })}
 	  </div>
 	);
@@ -52,9 +89,9 @@ class Cart extends React.Component {
 	
 }
 
-}
+      }
     
-
+      
 
 
 
