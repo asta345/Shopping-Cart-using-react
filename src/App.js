43cbaa91ff25d2a924c5225bf90dc1d958d73b0d
asta345 <81383112+asta345@users.noreply.github.com>
2021-10-06 15:00:37 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,102 +1,119 @@
-
 import React from 'react';
-
 import Cart from './Cart';
-
 import Navbar from './Navbar';
+
 class App extends React.Component {
-    constructor () {
-      super();
-      this.state = {
-        products:[
-          {
-            price:999,
-            title:'watch',
-            qty:1,
-           img:'https://media.istockphoto.com/photos/wrist-watch-picture-id180844253',
-            id: 1
-          },
-        
-          {
-            price:1099,
-            title:'phone',
-            qty:1,
-            img:'https://images.unsplash.com/photo-1591337676887-a217a6970a8a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80',
-            id:2
-          },
-          {
-            price:500,
-            title:'tablet',
-            qty:1,
-            img:'https://images.unsplash.com/photo-1453928582365-b6ad33cbcf64?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1173&q=80',
-            id:3
-          },
-        
+
+  constructor () {
+    super();
+    this.state = {
+      products: [
+        {
+          price:999,
+          title:'watch',
+          qty:1,
+         img:'https://media.istockphoto.com/photos/wrist-watch-picture-id180844253',
+          id: 1
+        },
       
-         
-        ]
-      }
-      //here we are binding object to to class to remove undefined error
-      // this.increaseQuantity = this.increaseQuantity.bind(this);
-      //  this.testing();
-          }
-          //to incrse the quantity of the cart item
-          handleIncreseQuantity =(product)=>{
-      console.log('heyyplease increse the product ',product);
-      const{products}=this.state;
-      const index = products.indexOf(product);
-      products[index].qty+=1;
-      this.setState({
-        products
-      })
-      }
-      handleDecreaseQuantity =(product)=>{
-        console.log('heyyplease decrese the product ',product);
-        const{products}=this.state;
-        const index = products.indexOf(product);
-        if(products[index].qty===0){
-          return
-        }
-        products[index].qty-=1;
-  
-        this.setState({ //setState is being used in onclick and callback events
-          products
-        })
-        }
-  
-      handleDeleteProduct =(id)=>{
-        
-        const {products}=this.state;
-        const items = products.filter((item) => item.id !== id); // [{}]
-  
-        this.setState({
-          products: items
-        })
-        
-      }
-      //code for adding to prices
-      getCartTotal=()=>{
-        const{products}=this.state;
-                      let cartTotal =0;
-  
-          products.map=((product) => {
-                       cartTotal = cartTotal+product.qty*product.price
-          })
-          return cartTotal;
-      }
-  render (){
-    return(
-  
-    <div className="App">
+        {
+          price:1099,
+          title:'phone',
+          qty:1,
+          img:'https://images.unsplash.com/photo-1591337676887-a217a6970a8a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80',
+          id:2
+        },
+        {
+          price:500,
+          title:'tablet',
+          qty:1,
+          img:'https://images.unsplash.com/photo-1453928582365-b6ad33cbcf64?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1173&q=80',
+          id:3
+        },
       
-      <Navbar/>
-      <Cart
-     onIncreaseQuantity={this.handleIncreseQuantity}
-     onDecreaseQuantity={this.handleDecreaseQuantity}
-     onDeleteProduct={this.handleDeleteProduct} />
-    </div>
-  );
-}
+    
+       
+      
+      ]
+    }
+    // this.increaseQuantity = this.increaseQuantity.bind(this);
+    // this.testing();
+  }
+  handleIncreaseQuantity = (product) => {
+    console.log('Heyy please inc the qty of ', product);
+    const { products } = this.state;
+    const index = products.indexOf(product);
+
+    products[index].qty += 1;
+
+    this.setState({
+      products
+    })
+  }
+  handleDecreaseQuantity = (product) => {
+    console.log('Heyy please inc the qty of ', product);
+    const { products } = this.state;
+    const index = products.indexOf(product);
+
+    if (products[index].qty === 0) {
+      return;
+    }
+
+    products[index].qty -= 1;
+
+    this.setState({
+      products
+    })
+  }
+  handleDeleteProduct = (id) => {
+    const { products } = this.state;
+
+    const items = products.filter((item) => item.id !== id); // [{}]
+
+    this.setState({
+      products: items
+    })
+  }
+  //to count cart items
+  getCartCount = () => {
+    const { products } = this.state;
+
+    let count = 0;
+
+    products.forEach((product) => {
+      count += product.qty;
+    })
+
+    return count;
+  } 
+  //to get cart total
+  getCartTotal = () => {
+    const { products } = this.state;
+
+    let CartTotal = 0;
+
+    products.map((product) => {
+      CartTotal = CartTotal + product.qty * product.price
+    })
+
+    return CartTotal;
+  }
+  render () {
+    const { products } = this.state;
+    return (
+      <div className="App">
+        <Navbar count={this.getCartCount()} />
+        <Cart
+          products={products}
+          onIncreaseQuantity={this.handleIncreaseQuantity}
+          onDecreaseQuantity={this.handleDecreaseQuantity}
+          onDeleteProduct={this.handleDeleteProduct}
+        />
+        <div style={{padding:10,fontSize:20}}>TOTAL:{this.getCartTotal()}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
