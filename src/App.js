9a542932d38a098cd3069e08ea95ed7f71d044f3
asta345 <81383112+asta345@users.noreply.github.com>
2021-10-13 14:48:38 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,46 +1,57 @@
-import React from 'react';
-import Cart from './Cart';
-import Navbar from './Navbar';
+import React from "react";
+import "./App.css";
+//  import CartItem from './CartItem';
+import Cart from "./Cart";
+import Navbar from "./Navbar";
+// import * as firebase from 'firebase';
+
 
 class App extends React.Component {
-
-  constructor () {
+  constructor() {
     super();
     this.state = {
       products: [
-        {
-          price:999,
-          title:'watch',
-          qty:1,
-         img:'https://media.istockphoto.com/photos/wrist-watch-picture-id180844253',
-          id: 1
-        },
+        // {
+        //   price:999,
+        //   title:'watch',
+        //   qty:1,
+        //  img:'https://media.istockphoto.com/photos/wrist-watch-picture-id180844253',
+        //   id: 1
+        // },
       
-        {
-          price:1099,
-          title:'phone',
-          qty:1,
-          img:'https://images.unsplash.com/photo-1591337676887-a217a6970a8a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80',
-          id:2
-        },
-        {
-          price:500,
-          title:'tablet',
-          qty:1,
-          img:'https://images.unsplash.com/photo-1453928582365-b6ad33cbcf64?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1173&q=80',
-          id:3
-        },
+        // {
+        //   price:1099,
+        //   title:'phone',
+        //   qty:1,
+        //   img:'https://images.unsplash.com/photo-1591337676887-a217a6970a8a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80',
+        //   id:2
+        // },
+        // {
+        //   price:500,
+        //   title:'tablet',
+        //   qty:1,
+        //   img:'https://images.unsplash.com/photo-1453928582365-b6ad33cbcf64?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1173&q=80',
+        //   id:3
+        // },
       
     
        
       
       ]
-    }
-    // this.increaseQuantity = this.increaseQuantity.bind(this);
-    // this.testing();
+        
+        
+    };
   }
-  handleIncreaseQuantity = (product) => {
-    console.log('Heyy please inc the qty of ', product);
+  //  componentDidMount(){
+  //   //  firebase
+  //   //  .firestore()
+  //    .collection('products')
+  //    .get()
+  //    .then((snapshot)=>{
+  //      console.log('snap')
+  //    })
+  //  }
+  handleIncreaseQuantity = product => {
     const { products } = this.state;
     const index = products.indexOf(product);
 
@@ -48,36 +59,35 @@ class App extends React.Component {
 
     this.setState({
       products
-    })
-  }
-  handleDecreaseQuantity = (product) => {
-    console.log('Heyy please inc the qty of ', product);
+    });
+  };
+
+  handleDecreaseQuantity = product => {
     const { products } = this.state;
     const index = products.indexOf(product);
 
     if (products[index].qty === 0) {
       return;
     }
-
     products[index].qty -= 1;
 
     this.setState({
       products
-    })
-  }
-  handleDeleteProduct = (id) => {
+    });
+  };
+
+  handleDeleteProduct = id => {
     const { products } = this.state;
 
-    const items = products.filter((item) => item.id !== id); // [{}]
+    const items = products.filter(product => product.id !== id);
 
     this.setState({
       products: items
-    })
-  }
+    });
+  };
   //to count cart items
-  getCartCount = () => {
+  getcountOfCartItems = () => {
     const { products } = this.state;
-
     let count = 0;
 
     products.forEach(product => {
@@ -86,37 +96,38 @@ class App extends React.Component {
 
     return count;
   };
-  //to get cart total
-  getCartTotal = () => {
+  // to get cart total
+  getcartTotal = () => {
     const { products } = this.state;
+    let cartTotal = 0;
 
-    let CartTotal = 0;
-
-    products.map((product) => {
-      if(product.qty>0){
-      CartTotal = CartTotal + product.qty * product.price
+    products.map(product => {
+      if (product.qty > 0) {
+        cartTotal = cartTotal + product.qty * product.price;
       }
-      return '';
-    })
+      return "";
+    });
 
-    return CartTotal;
-  }
-  render () {
+    return cartTotal;
+  };
+
+  render() {
     const { products } = this.state;
     return (
       <div className="App">
-        <Navbar count={this.getCartCount()} />
+        <Navbar count={this.getcountOfCartItems()} />
         <Cart
-          products={products}
           onIncreaseQuantity={this.handleIncreaseQuantity}
           onDecreaseQuantity={this.handleDecreaseQuantity}
           onDeleteProduct={this.handleDeleteProduct}
+          products={products}
         />
-        <div style={{padding:10,fontSize:20}}>TOTAL:{this.getCartTotal()}</div>
+        <div style={{ padding: 10, fontSize: 20 }}>
+          TOTAL : {this.getcartTotal()}
+        </div>
       </div>
     );
   }
 }
 
 export default App;
-
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 // import * as serviceWorker from "./serviceWorker";
 
-import "firebase/firestore";
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
